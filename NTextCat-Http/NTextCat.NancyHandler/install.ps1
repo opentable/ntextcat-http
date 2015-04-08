4da46f2e9c48dd08c41884fff40dfbf65994077d
daffers <dafydd.giddins@live.co.uk>
2015-04-08 16:18:34 +0100

    Adding a nuget project to build the package

---
+++ NTextCat-Http/NTextCat.NancyHandler/install.ps1
@@ -0,0 +1,9 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$configFile = $project.ProjectItems.Item("Core14.profile.xml")
+$copyToOutput2 = $configFile.Properties.Item("CopyToOutputDirectory")
+$copyToOutput2.Value = 1
+
+$webPage = $project.ProjectItems.Item("Views").ProjectItems.Item("language-detection-test-page.html")
+$copyToOutput1 = $webPage.Properties.Item("CopyToOutputDirectory")
+$copyToOutput1.Value = 1
