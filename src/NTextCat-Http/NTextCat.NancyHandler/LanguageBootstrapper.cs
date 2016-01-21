49f43fcf696ba5fec17bd7a9523f356e154a3ace
Seif Attar <iam@seifattar.net>
2016-01-21 14:51:58 +0000

    Use single instance of Identifier
    Was hitting performance problems on mono otherwise.
    Create the identifier in the contructor and ensure LanguageDetector is
    singleton.

---
+++ src/NTextCat-Http/NTextCat.NancyHandler/LanguageBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Nancy;
+using Nancy.TinyIoc;
+using NTextCat.NancyHandler.LanguageDetection;
+
+namespace NTextCat.NancyHandler
+{
+    public class LanguageBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            container.Register<LanguageDetector>().AsSingleton();
+        }
+    }
+}
+
